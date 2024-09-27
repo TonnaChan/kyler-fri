f334475f96b488a7cbc0bd6689cf45e351ef1d78
TonnaChan <63247372+TonnaChan@users.noreply.github.com>
2024-09-27 07:08:32 +0000

    20240927 Created a timestable program

---
+++ archive.py
@@ -0,0 +1,8 @@
+# name = "Alfred"
+# surname = "Chan"
+
+# print(name + " " + surname)
+
+# userName = input("What is your name? ")
+# lastName = input("What is your last name? ")
+# print(userName + " " + lastName)
